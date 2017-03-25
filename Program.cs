4410c3df065d919d1a394217952bf8d2b46c0cdb
dennis <king@hiberniagaming.net>
2017-03-25 03:26:27 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace patcher
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "dll files (*.dll)|*.dll";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    byte[] bytes = File.ReadAllBytes(dialog.FileName);
+                    byte[] search = new byte[] { 0x81, 0xCF, 0x00, 0x00, 0xCF, 0x00 };
+                    byte[] replace = new byte[] { 0xBF, 0x00, 0x00, 0x08, 0x15, 0x90 };
+                    int match = 0;
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        if (bytes[i] == search[match])
+                        {
+                            match++;
+                            if (match == 6)
+                            {
+                                for (int j = 0; j < match; j++)
+                                    bytes[i - match + j + 1] = replace[j];
+                                break;
+                            }
+                        }
+                        else
+                            match = 0;
+                    }
+                    File.WriteAllBytes(dialog.FileName, bytes);
+                    MessageBox.Show("patch successful");
+                    return;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Banished-BorderlessPatcher
+
+This is an updated version of the patcher created by dot and posted here (http://shiningrocksoftware.com/forum/discussion/537/release-native-borderless-fullscreen-patch#Item_1)
+
+#What do I need?
+
+You'll need eiter the .net framework version 2 or higher for the dotnet2.0 executable, and atleast .net framework 4.5.2 for the dotnet4.5.2 executable.
+
+#How to use?
+
+Download the latest release and patch Runtime-steam-x32.dll if your computer is x86 and Runtime-steam-x64.dll if your computer is x64.
+I think the gog files are named something different, Runtime-x32.dll and Runtime-x64.dll.
+
+After applying this patch you can then use windowed borderless gaming(http://westechsolutions.net/sites/WindowedBorderlessGaming/) or borderless gaming(https://github.com/Codeusa/Borderless-Gaming)to adjust the resolution.
+
+
+#What was changed?
+
+The value the orginal patcher looked for has changed.
+
+
+For anyone curious the function to look for is ?AdjustDisplay@Device@Video@@QAEXXZ void __thiscall Video::Device::AdjustDisplay(Video::Device *this)
+
+Look for the value being modified before being passed to SetWindowLongW
